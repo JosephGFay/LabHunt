5519cf8803d08be7f4d0e77072a3c0e070b04862
Joseph <tehbuildar@gmail.com>
2023-04-06 13:05:14 -0500

    added randomized npc names

--- windows/tableWindow.py
+++ windows/tableWindow.py
@@ -13,18 +13,22 @@ class TableWindow(InteractiveWindow):
         self.col_2 = self.x + 45 * 7
         self.col_3 = self.x + 45 * 10
 
-        self.row_0 = self.y + 40
+        self.row_0 = self.y + 80
         self.row_1 = self.y + 40 * 5
+        self.row_2 = self.row_1 + 40 * 5
 
         self.ui_objects = [
-            UIInteractableObject(self.col_0, self.row_0, 100, 120, 'assets/npc_top.png'),
-            UIInteractableObject(self.col_1, self.row_0, 100, 120, 'assets/npc_top.png'),
-            UIInteractableObject(self.col_2, self.row_0, 100, 120, 'assets/npc_top.png'),
-            UIInteractableObject(self.col_3, self.row_0, 100, 120, 'assets/npc_top.png'),
-
-            TextObject(None, table.npc_top.name, self.col_0 + 30, self.row_1),
-            TextObject(None, table.npc_right.name, self.col_1 + 30, self.row_1),
-            TextObject(None, table.npc_bottom.name, self.col_2 + 25, self.row_1),
-            TextObject(None, table.npc_left.name, self.col_3 + 25, self.row_1),
+            # Title
+            TextObject(None, 'Select A Student', self.col_0 + 100, self.row_0, 36),
+            # Player cards
+            UIInteractableObject(self.col_0, self.row_1, 100, 120, 'assets/npc_top.png'),
+            UIInteractableObject(self.col_1, self.row_1, 100, 120, 'assets/npc_top.png'),
+            UIInteractableObject(self.col_2, self.row_1, 100, 120, 'assets/npc_top.png'),
+            UIInteractableObject(self.col_3, self.row_1, 100, 120, 'assets/npc_top.png'),
+            # Names
+            TextObject(None, table.npc_top.name, self.col_0 + 30, self.row_2),
+            TextObject(None, table.npc_right.name, self.col_1 + 30, self.row_2),
+            TextObject(None, table.npc_bottom.name, self.col_2 + 25, self.row_2),
+            TextObject(None, table.npc_left.name, self.col_3 + 25, self.row_2),
         ]
         self.populate_menu_options(self.ui_objects)
